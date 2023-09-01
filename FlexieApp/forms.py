02d5b9cbfab7835c1f1dc8f9029906fbd909ab15
Morris Kagiri <morriskagiri@gmail.com>
2023-09-01 20:55:11 +0300

    get file uploads from users

--- FlexieApp/forms.py
+++ FlexieApp/forms.py
@@ -4,4 +4,5 @@ class UserInput(forms.Form):
     username = forms.CharField()
     email = forms.EmailField() 
     password = forms.CharField(widget=forms.PasswordInput)
-    
+    file = forms.FileField()
+
