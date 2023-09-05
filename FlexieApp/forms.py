f07b8ab8c41c951ccb800309c0dee3f9b66a7ba0
AgnesNM <agnesndutamwaniki@gmail.com>
2023-09-05 08:00:01 +0300

    edit form using srispy form helpers

--- FlexieApp/forms.py
+++ FlexieApp/forms.py
@@ -5,9 +5,23 @@ from django import forms
 #     email = forms.EmailField() 
 #     password = forms.CharField(widget=forms.PasswordInput)
 
+from crispy_forms.helper import FormHelper
 
+from crispy_forms.layout import Submit
 
 class UserInput(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-detectFraud'
+        self.helper.form_class = 'detectForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'detect fraud'
+
+        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.add_input(Submit('learn more', 'Learn More'))
+
+
     username = forms.CharField(
         label = "username",
         max_length = 80,
@@ -26,5 +40,6 @@ class UserInput(forms.Form):
         max_length = 80,
         required = True,
     )
+
 
     
