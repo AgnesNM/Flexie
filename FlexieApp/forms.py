455fad18115788722cef1f9c6fec9a4a57eb6cde
AgnesNM <agnesndutamwaniki@gmail.com>
2023-09-05 07:09:32 +0300

    edit email UI with helpers

--- FlexieApp/forms.py
+++ FlexieApp/forms.py
@@ -1,7 +1,30 @@
 from django import forms
 
+# class UserInput(forms.Form):
+#     username = forms.CharField()
+#     email = forms.EmailField() 
+#     password = forms.CharField(widget=forms.PasswordInput)
+
+
+
 class UserInput(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField() 
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        label = "username",
+        max_length = 80,
+        required = True,
+    )
+
+    email = forms.EmailField(
+        label = "email",
+        max_length = 80,
+        required = True,
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput,
+        label = "password",
+        max_length = 80,
+        required = True,
+    )
+
     
